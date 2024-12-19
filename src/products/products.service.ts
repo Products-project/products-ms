263e1c6730ede64164a916aa4b2d34de08bb423f
leonardo <quevedoruiz41@gmail.com>
2024-12-19 14:30:57 -0500

    Modify success returns

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -61,27 +61,19 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   async findOne(id: number) {
-    try {
-      const product = await this.products.findUnique({
-        where: {
-          id,
-          active: true,
-        },
-      });
-
-      if (!product)
-        throw new RpcException({
-          message: 'Product not found',
-          status: HttpStatus.NOT_FOUND,
-        });
-
-      return product;
-    } catch (error) {
+    const product = await this.products.findUnique({
+      where: {
+        id,
+        active: true,
+      },
+    });
+    if (!product)
       throw new RpcException({
-        message: `Internal server error ${error}`,
-        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Product not found',
+        status: HttpStatus.NOT_FOUND,
       });
-    }
+
+    return product;
   }
 
   async update(updateProductDto: UpdateProductDto) {
@@ -89,10 +81,23 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     await this.findOne(id);
 
     try {
-      return await this.products.update({
+      const updatedProduct = await this.products.update({
         where: { id },
         data,
       });
+
+      if (!updatedProduct) {
+        throw new RpcException({
+          message: `Error whent trying to update product`,
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+        });
+      }
+
+      return {
+        status: 200,
+        message: 'Product updated',
+        data: updatedProduct,
+      };
     } catch (error) {
       throw new RpcException({
         message: `Internal server error ${error}`,
@@ -105,10 +110,23 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     await this.findOne(id);
 
     try {
-      return await this.products.update({
+      const deletedProduct = await this.products.update({
         where: { id },
         data: { active: false },
       });
+
+      if (!deletedProduct) {
+        throw new RpcException({
+          message: `Error whent trying to delete product`,
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+        });
+      }
+
+      return {
+        status: 200,
+        message: 'Product deleted',
+        data: deletedProduct,
+      };
     } catch (error) {
       throw new RpcException({
         message: `Internal server error ${error}`,
